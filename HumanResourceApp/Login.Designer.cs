2c45dd95523edfc5a3e2b6932d49304f86ffb685
Sulakshana <deoresulakshana@gmail.com>
2025-12-02 16:35:48 +0530

    try catch finally

--- HumanResourceApp/Login.Designer.cs
+++ HumanResourceApp/Login.Designer.cs
@@ -29,11 +29,15 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
+            groupBox1 = new GroupBox();
+            textBox1 = new TextBox();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(222, 134);
+            button1.Location = new Point(89, 109);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 0;
@@ -41,19 +45,53 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // button2
+            // 
+            button2.Location = new Point(63, 139);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Location = new Point(414, 128);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(293, 214);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(63, 43);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 2;
+            // 
             // Login
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
             Controls.Add(button1);
             Name = "Login";
             Text = "Login";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Button button1;
+        private Button button2;
+        private GroupBox groupBox1;
+        private TextBox textBox1;
     }
 }
--- HumanResourceApp/Login.cs
+++ HumanResourceApp/Login.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,23 +22,61 @@ namespace HumanResourceApp
         private void button1_Click(object sender, EventArgs e)
         {
             try
-             {
-
-            
-            Maths m = new Maths();
-            int[] arr = new int[5] { 1, 2, 777334,4,4 };
-            int[] Answers=m.MultiplyNos(arr);
-            foreach (var item in Answers)
             {
-                MessageBox.Show(item.ToString());
-            }
+
+
+                Maths m = new Maths();
+                int[] arr = new int[5] { 1, 2, 777334, 4, 4 };
+                int[] Answers = m.MultiplyNos(arr);
+                foreach (var item in Answers)
+                {
+                    MessageBox.Show(item.ToString());
+                }
             }
             catch (Exception ex)
             {
 
-               MessageBox.Show(ex.Message);//message for the exception
+                MessageBox.Show(ex.Message);//message for the exception
                 MessageBox.Show(ex.Source);//where the excecption occured, ie library name(Logic place)
                 MessageBox.Show(ex.StackTrace);//line numbers from both the aps, ie windows app and libray also
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //int number = int.Parse(textBox1.Text);
+                //MessageBox.Show($"You entered {number}");
+
+                string s = textBox1.Text;
+                if (s.Length>0)
+                {
+                    MessageBox.Show(s.ToLower());
+                    MessageBox.Show(s.ToUpper());
+
+                   string finalstr = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(s);
+                    MessageBox.Show(s);
+
+                    //string[] words=s.Split(' ');
+                    //for (int i = 0; i < words.Length; i++)
+                    //{
+                    //words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1);
+                    //}
+                    //string resultantString = string.Join(" ", words);
+                    //MessageBox.Show(resultantString);
+                }
+            }
+            catch (FormatException ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally 
+            {
+                //clsoing connection,releasing resources, cleaning of logs....
+                MessageBox.Show("Finally executed whether exception fired or not");
+
             }
         }
     }
