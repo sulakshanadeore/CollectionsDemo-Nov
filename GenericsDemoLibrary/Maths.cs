1e10216681d17f464c44afff30ecdc4247b66d42
Sulakshana <deoresulakshana@gmail.com>
2025-12-02 15:24:51 +0530

    stack trace, source and message

--- GenericsDemoLibrary/Maths.cs
+++ GenericsDemoLibrary/Maths.cs
@@ -24,5 +24,34 @@ namespace GenericsDemoLibrary
             return ans;
         }
 
+        public int[] MultiplyNos(int[] arr)
+        {
+            int[] ans = new int[3];
+            try
+            {
+                if (arr.Length > 3)
+                {
+                    throw new IndexOutOfRangeException("Array too long");
+                 }
+                else
+                {
+                  for (int i = 0; i < 3; i++)
+                    {
+                        ans[i] = arr[i] * 1000000;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+             //   throw new Exception("Failed to read array, size error",ex);
+            }
+
+            return ans;
+
+        }
+
+
+
     }
 }
---
+++ HumanResourceApp/Login.cs
@@ -0,0 +1,44 @@
+﻿using GenericsDemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HumanResourceApp
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+             {
+
+            
+            Maths m = new Maths();
+            int[] arr = new int[5] { 1, 2, 777334,4,4 };
+            int[] Answers=m.MultiplyNos(arr);
+            foreach (var item in Answers)
+            {
+                MessageBox.Show(item.ToString());
+            }
+            }
+            catch (Exception ex)
+            {
+
+               MessageBox.Show(ex.Message);//message for the exception
+                MessageBox.Show(ex.Source);//where the excecption occured, ie library name(Logic place)
+                MessageBox.Show(ex.StackTrace);//line numbers from both the aps, ie windows app and libray also
+            }
+        }
+    }
+}
